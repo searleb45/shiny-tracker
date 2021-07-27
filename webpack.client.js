d7bcda7af72d055d4b5f4ace9f6bb17da5e98215
Brad Searle <b.searle@outlook.com>
2021-07-27 16:07:05 -0500

    Update fetching of keys for dev/prod

--- webpack.client.js
+++ webpack.client.js
@@ -6,19 +6,26 @@ const dotenv = require('dotenv');
 
 module.exports = (env, argv) => {
 	let envKeys = {};
-	if(argv.mode === 'development') {
-		const parsedEnv = dotenv.config().parsed;
-	
-		envKeys = Object.keys(parsedEnv).reduce((prev, next) => {
-			prev[`process.env.${next}`] = JSON.stringify(parsedEnv[next]);
-			return prev;
-		}, {})
+	switch(argv.mode) {
+		case 'development':
+			console.log('pulling dev keys from dotenv');
+			const parsedEnv = dotenv.config().parsed;
+		
+			envKeys = Object.keys(parsedEnv).reduce((prev, next) => {
+				prev[`process.env.${next}`] = JSON.stringify(parsedEnv[next]);
+				return prev;
+			}, {});
+			break;
+		case 'production':
+			console.log('pulling prod keys from Heroku config');
+			const clientKeys = ['TWITCH_CLIENT_ID'];
+		
+			envKeys = clientKeys.reduce((prev, next) => {
+				prev[`process.env.${next}`] = process.env[next];
+			}, {});
+			break;
 	}
-	if(argv.mode === 'production') {
-		const clientKeys = ['TWITCH_CLIENT_ID'];
 
-		envKeys = clientKeys.map(key => process.env[key]);
-	}
 	return {
 		entry: './src/client/index.js',
 		output: {
