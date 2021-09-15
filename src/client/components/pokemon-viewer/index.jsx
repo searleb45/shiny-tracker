965f7618205e9c4a8460547e254db2db40c40092
Brad Searle <b.searle@outlook.com>
2021-09-14 20:11:30 -0500

    Nullcheck game for hisuian form logic

--- src/client/components/pokemon-viewer/index.jsx
+++ src/client/components/pokemon-viewer/index.jsx
@@ -12,14 +12,16 @@ const PokemonViewer = (props) => {
 	const pokemon = {...POKEMON_LIST.find((pkmn) => pkmn.id === pokemonId)};
 	const game = GAME_LIST.find((game) => game.gameId === gameId);
 
-	if(game && game.useAlolanForm && pokemon.hasAlolanForm) {
-		pokemon.sprite = pokemon.sprite.replace('.gif', '-alola.gif');
-	}
-	if(game && game.useGalarianForm && pokemon.hasGalarianForm) {
-		pokemon.sprite = pokemon.sprite.replace('.gif', '-galar.gif');
-	}
-	if(game.useHisuianForm && pokemon.hasHisuianForm) {
-		pokemon.sprite = pokemon.sprite.replace('.gif', '-hisui.gif');
+	if(game) {
+		if(game.useAlolanForm && pokemon.hasAlolanForm) {
+			pokemon.sprite = pokemon.sprite.replace('.gif', '-alola.gif');
+		}
+		if(game.useGalarianForm && pokemon.hasGalarianForm) {
+			pokemon.sprite = pokemon.sprite.replace('.gif', '-galar.gif');
+		}
+		if(game.useHisuianForm && pokemon.hasHisuianForm) {
+			pokemon.sprite = pokemon.sprite.replace('.gif', '-hisui.gif');
+		}
 	}
 
 	return (
