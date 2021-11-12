66c5adb241f4f740aef08cb5fb5ce3e998f1ce24
Brad Searle <b.searle@outlook.com>
2021-11-12 09:50:28 -0600

    Force SSL support on Heroku hosting instances

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import path from 'path';
 import cookieSession from 'cookie-session';
+import sslRedirect from 'heroku-ssl-redirect';
 
 import apiModule from './api';
 import authModule from './auth';
 
 const app = express();
+if(process.env.NODE_ENV === 'production') {
+	app.use(sslRedirect());
+}
 app.use(express.json());
 app.use(cookieSession({ keys: [process.env.SESSION_KEY], maxAge: 1000 * 60 * 60 * 24 * 365 }));
 
