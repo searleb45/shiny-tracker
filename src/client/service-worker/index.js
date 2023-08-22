7ec9aaf9f35f6a5fec1bf8b826c748cee8fb2cad
Brad Searle <b.searle@outlook.com>
2023-08-22 15:50:49 -0500

    Revert "Did I maybe fix the service worker updating issue? Please?"
    This reverts commit 622c000a4023fa307e3bcfcc243ad5943a0c229f.

--- src/client/service-worker/index.js
+++ src/client/service-worker/index.js
@@ -1,13 +1,15 @@
 import { NavigationRoute, registerRoute } from 'workbox-routing';
 import {CacheFirst, NetworkFirst} from 'workbox-strategies';
-import {precacheAndRoute} from 'workbox-precaching';
+import {precacheAndRoute, createHandlerBoundToURL} from 'workbox-precaching';
 import {openDB} from 'idb';
 
 self.skipWaiting();
 
 precacheAndRoute($WEBPACK_GENERATED_MANIFEST);
 
-registerRoute(new NavigationRoute(new NetworkFirst()));
+registerRoute(new NavigationRoute(createHandlerBoundToURL("index.html"), {
+	denylist: [/\/twitchAuth(\/.*)?/]
+}), new NetworkFirst());
 registerRoute(/\/(boxart|sprites)\/.*/, new CacheFirst(), 'GET');
 registerRoute(/\.(css|js)/, new CacheFirst(), 'GET');
 
