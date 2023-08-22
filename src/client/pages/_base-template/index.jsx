a0cce24c758836e4256e4152c03b2f6df9e69171
Brad Searle <b.searle@outlook.com>
2023-08-22 11:34:00 -0500

    Add scroll to top button

--- src/client/pages/_base-template/index.jsx
+++ src/client/pages/_base-template/index.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import TopIcon from '../../static/icons/top.svg?react';
 
 import './base-template.scss';
 
 const BasePageTemplate = (props) => {
 	const { header, page, className } = props;
+	const [scrollPos, setScrollPos] = useState(0);
+
+	useEffect(() => {
+		const handleScroll = () => {
+			console.log(window.scrollY, window.innerHeight);
+			setScrollPos(window.scrollY);
+		}
+
+		window.addEventListener('scroll', handleScroll, { passive: true });
+
+		return () => window.removeEventListener('scroll', handleScroll);
+	}, []);
 
 	return (
 		<main className={`base-page-template ${className}`}>
@@ -13,6 +27,15 @@ const BasePageTemplate = (props) => {
 			<div className="page">
 				{page}
 			</div>
+			{scrollPos > window.innerHeight * 2 && (
+				<button
+					className="btn-primary scrollTop"
+					onClick={() => window.scrollTo({top: 0, behavior: 'smooth'})}
+					aria-label="Scroll to top"
+				>
+					<TopIcon />
+				</button>
+			)}
 		</main>
 	)
 }
