974159c15fcc16b0aab50d8141b2d6609a3d5ba5
Brad Searle <b.searle@outlook.com>
2024-03-08 15:16:50 -0600

    Port database to Koyeb postgres instances since Planetscale is shutting down free DBs

--- src/server/db/index.js
+++ src/server/db/index.js
@@ -3,22 +3,30 @@ import Sequelize from 'sequelize';
 import createHuntModel from './models/hunt';
 import createShinydexModel from './models/shinydex';
 
-const [,user, password, host, db] = process.env.DATABASE_URL.match(/\/\/(.*):(.*)@(.*)\/(.*)\?.*/);
-
 let connection = {};
+let user, password, host, db;
+if (process.env.LOCAL_CONNECTION) {
+	[,user, password, host, db] = process.env.DATABASE_URL.match(/\/\/(.*):(.*)@(.*)\/(.*)\?.*/);
+} else {
+	user = process.env.DATABASE_USER;
+	password = process.env.DATABASE_PASSWORD;
+	host = process.env.DATABASE_HOST;
+	db = process.env.DATABASE_NAME;
+}
+
 if(user && password && host && db) {
 	connection = new Sequelize(db, user, password, {
 		host,
-		dialect: 'mysql',
-		dialectOptions: process.env.CLEARDB_DATABASE_URL ? undefined : {
+		dialect: process.env.LOCAL_CONNECTION ? 'mysql' : 'postgres',
+		dialectOptions: process.env.LOCAL_CONNECTION ? undefined : {
 			ssl: {
 				require: true
 			}
 		}
 	});
 
-	connection.hunt = createHuntModel(connection, Sequelize);
-	connection.shinydex = createShinydexModel(connection, Sequelize);
+	connection.hunt = createHuntModel(connection, Sequelize, process.env.LOCAL_CONNECTION);
+	connection.shinydex = createShinydexModel(connection, Sequelize, process.env.LOCAL_CONNECTION);
 }
 
 connection.authenticate().then(() => {
